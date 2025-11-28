8e5c101a39192e979ea4a5eec95f04ce75474b1d
Kumar-Amitesh <amitesh04kumar@gmail.com>
2025-11-28 21:17:24 +0530

    feat: enhance multer middleware to generate unique filenames for uploaded files

--- auth-service/src/middleware/multer.middleware.js
+++ auth-service/src/middleware/multer.middleware.js
@@ -1,11 +1,17 @@
 import multer from 'multer'
+import path from 'path'
 
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
         cb(null,'.././public/temp')
+        // path.resolve('/public/temp')
     },
     filename: function(req,file,cb){
-        cb(null,file.originalname)
+        // cb(null,file.originalname)
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const fileExtension = path.extname(file.originalname);
+
+        cb(null, `${uniqueSuffix}${fileExtension}`)
     }
 })
 
