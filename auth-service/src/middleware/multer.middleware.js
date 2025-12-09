f50d81d706a8d693ebe590fd66c5ddcede0ac3e0
Kumar-Amitesh <amitesh04kumar@gmail.com>
2025-12-09 19:00:05 +0530

    fix(multer): update upload path handling and ensure directory creation

--- auth-service/src/middleware/multer.middleware.js
+++ auth-service/src/middleware/multer.middleware.js
@@ -1,10 +1,19 @@
 import multer from 'multer'
 import path from 'path'
+import fs from 'fs'
 
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null,'.././public/temp')
+        // cb(null,'.././public/temp')
         // path.resolve('/public/temp')
+        const uploadPath = path.join(process.cwd(), 'public', 'temp'); 
+        // console.log('Upload Path:', uploadPath);
+        // Check if directory exists, if not, create it
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+
+        cb(null, uploadPath)
     },
     filename: function(req,file,cb){
         // cb(null,file.originalname)
